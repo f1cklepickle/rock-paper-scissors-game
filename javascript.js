39341a84cea71a58d2219948ecf87710e30a01ab
Harrison B <harrisonbruhl@gmail.com>
2024-08-20 18:06:36 -0400

    feat: add player choice function

--- javascript.js
+++ javascript.js
@@ -3,19 +3,29 @@ function getComputerChoice() {
 
     if (choice <= 33){
         return ('rock')
-    }
-
-    else if (choice <= 66) {
+    } else if (choice <= 66) {
          return ('paper')
-        }
-
-    else if (choice <= 99){
+    } else if (choice <= 99){
          return ('scissors') 
-        }
-
-         
-
-    else {getComputerChoice()
+    } else {
+        getComputerChoice()
     }
+}
 
+function getPlayerChoice() {
+    let answer = prompt('Chose Rock, Paper, or Scissors.')
+
+    if (answer.toLowerCase() == 'rock') {
+        return answer;
+    } else if (answer.toLowerCase() == 'paper') {
+        return answer;
+    } else if (answer.toLowerCase() == 'scissors') {
+        return answer;
+    } else if (answer == '' || null) {
+        alert('Make a choice!') 
+        getPlayerChoice()
+    } else {
+       alert('Not a valid answer.') 
+       return;
+    }
 }
