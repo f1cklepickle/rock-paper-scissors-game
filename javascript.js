7c325bf4c29ae9bbcacf2ac222a3db2bf6f0ffb2
Harrison B <harrisonbruhl@gmail.com>
2024-08-28 18:56:37 -0400

    feat: add playGame function and score counter - playerScore and computerScore store rounds won - playRound nested inside playGame compares playerChoice to computerChoice then returns a winner - playGame gets playerChoice and compuerChoice then calls playRound until a game winner is determined

--- javascript.js
+++ javascript.js
@@ -1,31 +1,66 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 100) + 1;
-
     if (choice <= 33){
-        return ('rock')
+        let choice = ('rock');
+        return choice;
     } else if (choice <= 66) {
-         return ('paper')
+        let choice = ('paper');
+         return choice;
     } else if (choice <= 99){
-         return ('scissors') 
+        let choice = ('scissors');
+         return choice;
     } else {
-        getComputerChoice()
+        return getComputerChoice();
     }
 }
 
 function getPlayerChoice() {
-    let answer = prompt('Chose Rock, Paper, or Scissors.')
-
-    if (answer.toLowerCase() == 'rock') {
-        return answer;
-    } else if (answer.toLowerCase() == 'paper') {
-        return answer;
-    } else if (answer.toLowerCase() == 'scissors') {
-        return answer;
-    } else if (answer == '' || null) {
-        alert('Make a choice!') 
-        getPlayerChoice()
+    let choice = prompt('Chose Rock, Paper, or Scissors.');
+    if (choice.toLowerCase() == 'rock') {
+        return choice;
+    } else if (choice.toLowerCase() == 'paper') {
+        return choice;
+    } else if (choice.toLowerCase() == 'scissors') {
+        return choice;
+    } else if (choice == '' || null) {
+        alert('Make a choice!')
+        return getPlayerChoice();
     } else {
-       alert('Not a valid answer.') 
-       return;
+       return getPlayerChoice();
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame() {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    let reset = playRound(playerChoice.toLowerCase(), computerChoice.toLowerCase());
+    if (playerScore == 3) {
+        console.log(reset)
+        return `Game over. You win! Score:${playerScore}-${computerScore}`;
+    } else if (computerScore == 3){
+        console.log(reset)
+        return `Game over. You lose! Score:${playerScore}-${computerScore}`;
+    } else {
+        console.log(reset)
+        return playGame();
+    }
+
+function playRound(playerChoice, computerChoice) {
+        if (playerChoice === 'rock' && computerChoice === 'scissors' 
+            || playerChoice === 'paper' && computerChoice === 'rock' 
+            || playerChoice === 'scissors' && computerChoice === 'paper') {
+            playerScore++
+            return `You Win! The computer chose ${computerChoice}.`
+        } else if (playerChoice === undefined || computerChoice === undefined) {
+            return `No contest.`;
+        } else if (playerChoice === computerChoice) {
+            return `Draw! The compuer chose ${computerChoice}.`;
+        } else {
+            computerScore++
+            return `You lose! The computer chose ${computerChoice}.`;
+        }
     }
 }
